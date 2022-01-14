5bde6cca55ae1ef37d5ccb0769615672eccd43b1
Keith LaForce <keith@hastearcade.com>
2022-01-13 23:54:12 -0500

    moving to mirror example as baseline with websocket as transport layer

---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"C:\Program Files\Unity\Hub\Editor\2020.3.26f\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\Keith\winprojects\UnityHasteIntegrationSample" -executeMethod BuildScript.BuildAll
+echo "Finished build!"
